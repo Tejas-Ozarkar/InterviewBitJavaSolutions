ecdc121f0032779c80231d56da7745bd9cc15b6d
Tejas-Ozarkar <tejozarkar@gmail.com>
2019-09-08 21:35:20 +0530

    Problem: Add One To Java

---
+++ Arrays/addOneToNumber.java
@@ -0,0 +1,38 @@
+public class Solution {
+    
+    public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
+        int i = A.size()-1;
+        
+        if(A.get(i)<9){
+            A.set(i, A.get(i)+1);
+        }else{
+            A.set(i, 0);
+            if(i==0){
+                A.add(0,1);
+            }else{
+                i--;
+                boolean addToLast = true;
+                while(A.get(i)==9){
+                    A.set(i,0);
+                    if(i==0){
+                        A.add(0,1);
+                        addToLast = false;
+                        break;
+                    }   
+                    i--;
+                      
+                }
+                if(addToLast)
+                    A.set(i, A.get(i)+1);
+                
+            }
+    
+        }
+        
+        while(A.get(0)==0){
+            A.remove(0);
+        }
+        
+        return A;
+    }
+}
