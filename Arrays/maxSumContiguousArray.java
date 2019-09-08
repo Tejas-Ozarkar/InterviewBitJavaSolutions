eb621a57dabf823c09d241b517830925959f20e7
Tejas-Ozarkar <tejozarkar@gmail.com>
2019-09-08 22:00:30 +0530

    Facebook

---
+++ Arrays/maxSumContiguousArray.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int maxSubArray(final List<Integer> A) {
+        int sum = 0, max=0, maxItem=-1001;
+        for(int i = 0;i<A.size();i++){
+            int currentValue = A.get(i);
+            if(currentValue>maxItem)
+                maxItem = currentValue;
+            sum+=currentValue;
+            if(sum<0){
+                sum = 0;
+            }
+            if(sum>max){
+                max = sum;
+            }
+        }
+        return max==0?maxItem:max;
+    }
+}
