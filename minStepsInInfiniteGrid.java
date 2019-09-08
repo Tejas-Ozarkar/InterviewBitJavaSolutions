bf2926f67c7bf5574eedeab9f16c058cf59371dc
Tejas-Ozarkar <tejozarkar@gmail.com>
2019-09-08 22:08:27 +0530

    Directi

---
+++ minStepsInInfiniteGrid.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int coverPoints(ArrayList<Integer> A, ArrayList<Integer> B) {
+        int steps = 0;
+        for(int i = 0;i<A.size()-1;i++){
+            steps += Math.max(Math.abs(A.get(i)-A.get(i+1)), Math.abs(B.get(i)-B.get(i+1)));        
+        }
+        return steps;
+    }
+}
